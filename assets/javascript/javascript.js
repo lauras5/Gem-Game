42083abe3e4234e5ba60d1d2e2124f1fb1c74f8b
Laura Salcido <idlaurasalcido@gmail.com>
2018-02-13 14:21:05 -0800

    fixed a bug

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -7,6 +7,8 @@ $(document).ready(function() {
   $(".wins").text("WINS : " + wins)
   var losses = 0
   $(".losses").text("LOSSES : " + losses)
+  var myImage = $("<img>")
+  
     
   //reset function
   function reset() {
@@ -14,44 +16,48 @@ $(document).ready(function() {
     $( "#numGuess" ).text( "Click the Gems to Match the Number Below!  " + randNum )
     total = 0
     $("#myNum").text(total)
-    // console.log("hello")
+    for ( var i = 0 ; i < 4; i++ ) {
+      myImage.attr("src", "assets/images/gem.png")
+      myImage.attr("class", "gemSize myGem") 
+      myImage.attr("data-gemValue", Math.floor( Math.random() * 10 ))
+      $("#gems").append(myImage)
+    }
   }
 
-  for ( var i = 0 ; i < 4; i++ ) {
+  $(".resetBtn").on("click", function() {
+    reset()
+    // console.log("hello")
+  })
+
+    for ( var i = 0 ; i < 4; i++ ) {
     var myImage = $("<img>")
     myImage.attr("src", "assets/images/gem.png")
     myImage.attr("class", "gemSize myGem")
     myImage.attr("data-gemValue", Math.floor( Math.random() * 10 ) )
     $("#gems").append(myImage)
   }
-    
+  
   $(".myGem").on("click", function() {
     var gemNum = parseInt($(this).attr("data-gemValue"))
-    // $(".amount").append(gemNum + " + ")
     total += gemNum
     $("#myNum").text(total)
     if(total === randNum) {
       alert("You Win!")
       wins++
-      $(".wins").text("WINS : " + wins)
+      $(".wins").push("WINS : " + wins)
+      reset()
     }
+
     else if(total > randNum) {
       alert("You Lose! Try again!")
       losses++
       $(".losses").text("LOSSES : " + losses)
       reset()
-    }
-    else {
-      return
-    }
-      
+    } 
   })
   
   //gets another number and resets 
-  $(".resetBtn").on("click", function() {
-    reset()
-    console.log("hello")
-  })
+  
 
 })
 
